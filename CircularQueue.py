f3162a1ed8b2b4f8f10cc37ea4a04e5fb4e097d0
karlygaw <karlygash.ussen@narxoz.kz>
2024-01-19 03:26:44 +0600

    circular queue

---
+++ CircularQueue.py
@@ -0,0 +1,65 @@
+class CircularQueue:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.front = self.rear = -1
+
+    def is_empty(self):
+        return self.front == -1
+
+    def is_full(self):
+        return (self.rear + 1) % self.capacity == self.front
+
+    def enqueue(self, item):
+        if self.is_full():
+            print("Queue is full. Cannot enqueue.")
+        else:
+            if self.is_empty():
+                self.front = self.rear = 0
+            else:
+                self.rear = (self.rear + 1) % self.capacity
+            self.queue[self.rear] = item
+            print(f"Enqueued: {item}")
+
+    def dequeue(self):
+        if self.is_empty():
+            print("Queue is empty. Cannot dequeue.")
+        else:
+            item = self.queue[self.front]
+            if self.front == self.rear:
+                self.front = self.rear = -1
+            else:
+                self.front = (self.front + 1) % self.capacity
+            print(f"Dequeued: {item}")
+
+    def display(self):
+        if self.is_empty():
+            print("Queue is empty.")
+        else:
+            print("Circular Queue:")
+            i = self.front
+            while True:
+                print(self.queue[i], end=" ")
+                if i == self.rear:
+                    break
+                i = (i + 1) % self.capacity
+            print()
+
+
+capacity = 5
+cq = CircularQueue(capacity)
+
+for i in range(1, capacity + 2):
+    cq.enqueue(i)
+
+cq.display()
+
+cq.dequeue()
+cq.dequeue()
+
+cq.display()
+
+cq.enqueue(6)
+cq.enqueue(7)
+
+cq.display()
